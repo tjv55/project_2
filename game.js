84ed0123164c860827e271b95083c330babfbe31
tjv55 <tjv55@nau.edu>
2016-05-23 22:41:38 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -15,16 +15,12 @@ var leftLower = new PIXI.Sprite(PIXI.Texture.fromImage("assets/leftlower.png"));
 var rightLower = new PIXI.Sprite(PIXI.Texture.fromImage("assets/rightlower.png"));
 var creditsScreen = new PIXI.Sprite(PIXI.Texture.fromImage("assets/creditsScreen.png"));
 var instructScreen = new PIXI.Sprite(PIXI.Texture.fromImage("assets/instructScreen.png"));
+var simonArray = [];
 
-//music loader
-PIXI.loader
-    .add("prologue.mp3")
-    .load(ready);
-var backgroundSound;
 
-function ready()
+function randomSimon(min, max)
 {
-  backgroundSound = PIXI.audioManager.getAudio("assets/prologue.mp3");
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 start.position.x = 250;
@@ -49,20 +45,34 @@ leftLower.position.x = 0;
 leftLower.position.y = 300;
 rightLower.position.x = 300;
 rightLower.position.y = 300;
-
-
+creditsScreen.position.x = -300;
+creditsScreen.position.y = -300;
+instructScreen.position.x = -300;
+instructScreen.position.y = -300;
+stage.addChild(background);
 stage.addChild(leftUpper);
 stage.addChild(rightUpper);
 stage.addChild(leftLower);
 stage.addChild(rightLower);
-stage.addChild(background);
+
 stage.addChild(creditsScreen);
 stage.addChild(instructScreen);
 stage.addChild(start);
 stage.addChild(credits);
 stage.addChild(instructs);
 stage.addChild(mainMenu);
+PIXI.loader
+    .add("assets/prologue.mp3")
+    .load(ready);
 
+var backgroundSound;
+//music loader
+
+
+function ready()
+{
+  backgroundSound = PIXI.audioManager.getAudio("assets/prologue.mp3");
+}
 //simon says stage loader
 
 instructScreen.visible = false;
@@ -86,7 +96,31 @@ function mouseStart(e)
   leftLower.visible = true;
   rightLower.visible = true;
   mainMenu.visible = true;
+  backgroundSound.play();
 
+
+  for(i = 0; i < 10; i++)
+  {
+    simonArray[i] = randomSimon(0,3);
+
+    if(simonArray[i] = 0)
+    {
+
+    }
+    if(simonArray[i] = 1)
+    {
+
+    }
+    if(simonArray[i] = 2)
+    {
+
+    }
+    if(simonArray[i] = 3)
+    {
+
+    }
+
+  }
 }
 
 function mouseCredits(e)
@@ -96,6 +130,7 @@ function mouseCredits(e)
   instructs.visible = false;
   creditsScreen.visible = true;
   mainMenu.visible = true;
+  createjs.Tween.get(creditsScreen.position).to({x: 0, y: 0}, 1000, createjs.Ease.bounceOut);
 
 }
 
@@ -106,6 +141,7 @@ function mouseInstructs(e)
   instructs.visible = false;
   mainMenu.visible = true;
   instructScreen.visible = true;
+  createjs.Tween.get(instructScreen.position).to({x: 0, y: 0}, 1000, createjs.Ease.bounceOut);
 }
 
 function mouseMenu(e)
@@ -121,7 +157,10 @@ function mouseMenu(e)
   rightLower.visible = false;
   leftUpper.visible = false;
   rightUpper.visible = false;
-
+  creditsScreen.position.x = -300;
+  creditsScreen.position.y = -300;
+  instructScreen.position.x = -300;
+  instructScreen.position.y = -300;
 }
 
 start.interactive = true;
@@ -133,24 +172,42 @@ instructs.on('mousedown', mouseInstructs);
 mainMenu.interactive = true;
 mainMenu.on('mousedown', mouseMenu);
 
+function mouseLeftU(e)
+{
+
+}
+function mouseLeftL(e)
+{
+
+}
+function mouseUpperU(e)
+{
+
+}
+function mouseUpperL(e)
+{
+
+}
 //simon says interaction
-// leftUpper.interactive = true;
-// leftUpper.on('mousedown', mouseMenu);
-//
-// leftLower.interactive = true;
-// leftLower.on('mousedown', mouseMenu);
-//
-// rightUpper.interactive = true;
-// rightUpper.on('mousedown', mouseMenu);
-//
-// rightLower.interactive = true;
-// rightLower.on('mousedown', mouseMenu);
+leftUpper.interactive = true;
+leftUpper.on('mousedown', mouseLeftU);
+
+leftLower.interactive = true;
+leftLower.on('mousedown', mouseLeftL);
+
+rightUpper.interactive = true;
+rightUpper.on('mousedown', mouseUpperU);
+
+rightLower.interactive = true;
+rightLower.on('mousedown', mouseUpperL);
 
 
 
-function animate() {
+function animate()
+{
   requestAnimationFrame(animate);
   renderer.render(stage);
+
 }
 
 animate();
